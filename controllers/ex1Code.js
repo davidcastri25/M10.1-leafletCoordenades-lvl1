736f86466531da505d4fd5b98668dbefc4aebd76
David Castrillón <david.castri@hotmail.es>
2021-07-14 18:37:30 +0200

    14/07/2021 - Primer intento leaflet

---
+++ controllers/ex1Code.js
@@ -0,0 +1,12 @@
+/* Fase 1 */
+var mapFase1 = L.map('mapFase1').setView([41.38715190920178, 2.1700741025685213], 15);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mapFase1);
+
